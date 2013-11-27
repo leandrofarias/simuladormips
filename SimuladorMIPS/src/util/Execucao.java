2ece12a17039902d00b8b52b86c294e196f9b38c
Sergio <lsergiobp@gmail.com>
2013-11-27 10:41:10 -0400

    Fluxograma dos sinais de controle.

---
+++ SimuladorMIPS/src/util/Execucao.java
@@ -0,0 +1,210 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import modelo.Add;
+import modelo.And;
+import modelo.Beq;
+import modelo.Instrucao;
+import modelo.J;
+import modelo.Lw;
+import modelo.Or;
+import modelo.Registrador;
+import modelo.Sub;
+import modelo.Sw;
+import modelo.Variavel;
+
+public class Execucao {
+	
+	private Montador m = new Montador();
+	private List<Variavel> lista = new ArrayList<Variavel>();
+	
+	private MemoriaInstrucoes memoriaInstrucoes = new MemoriaInstrucoes();
+	private BancoRegistradores bancoRegistradores = new BancoRegistradores();
+	private int quantidadeInstrucoes;
+	private HashMap< Integer, SinalControle > sinaisControle = new HashMap< Integer, SinalControle >();
+	
+	public void executar() {
+		
+		init();
+		
+		int instrucoesRealizadas = 0;
+		this.quantidadeInstrucoes = this.memoriaInstrucoes.getInstrucoes().size();
+		
+		
+		while ( instrucoesRealizadas < this.quantidadeInstrucoes ) {
+			System.out.println( "Buscando instrução..." );
+			this.sinaisControle.get( 4 ).modificarValor( "" );
+			this.sinaisControle.get( 7 ).modificarValor( "" );
+			this.sinaisControle.get( 2 ).modificarValor( "" );
+			this.sinaisControle.get( 11 ).modificarValor( "0" );
+			this.sinaisControle.get( 3 ).modificarValor( "0" );
+			this.sinaisControle.get( 10 ).modificarValor( "01" );
+			this.sinaisControle.get( 9 ).modificarValor( "00" );
+			this.sinaisControle.get( 8 ).modificarValor( "00" );
+			
+			this.imprimirSinaisDeControleAtivos();
+			this.inativarSinaisControle();
+			this.realizarInstrucao();
+		}
+	}
+	
+	private void realizarInstrucao() {
+		
+		//de algum jeito pega a 1a instrução
+		System.out.println("Decodificando instrução e buscando registrador");
+		this.sinaisControle.get( 11 ).modificarValor( "0" );
+		this.sinaisControle.get( 10 ).modificarValor( "11" );
+		this.sinaisControle.get( 9 ).modificarValor( "00" );
+		
+		this.imprimirSinaisDeControleAtivos();
+		this.inativarSinaisControle();
+		
+		//de algum jeito vai achar qual tipo de instrução
+		Instrucao instrucao = new Instrucao();
+		if( instrucao instanceof Add || instrucao instanceof Sub || instrucao instanceof And || instrucao instanceof Or )
+			instrucaoTipoR();
+		else if( instrucao instanceof Lw || instrucao instanceof Sw ) 
+			instrucaoTipoLoadStore( instrucao );
+		else if( instrucao instanceof Beq ) 
+			instrucaoTipoBeq();
+		else if( instrucao instanceof J ) 
+			instrucaoTipoJ();
+	}
+		
+	private void instrucaoTipoR(){
+		
+		System.out.println( "Execução da instrucao tipo R" );
+		
+		this.sinaisControle.get( 11 ).modificarValor( "1" );
+		this.sinaisControle.get( 10 ).modificarValor( "00" );
+		this.sinaisControle.get( 9 ).modificarValor( "10" );
+		
+		this.imprimirSinaisDeControleAtivos();
+		this.inativarSinaisControle();
+		
+		//Executa tudo...
+		System.out.println("Termino da instrução");
+		this.sinaisControle.get( 13 ).modificarValor( "1" );
+		this.sinaisControle.get( 12 ).modificarValor( "" );
+		this.sinaisControle.get( 6 ).modificarValor( "0" );
+		
+		this.imprimirSinaisDeControleAtivos();
+		this.inativarSinaisControle();
+		
+	}
+	
+	private void instrucaoTipoLoadStore( Instrucao instrucao ) {
+		
+		System.out.println( "Calculo do endereco de memoria" );
+		this.sinaisControle.get( 11 ).modificarValor( "1" );
+		this.sinaisControle.get( 10 ).modificarValor( "10" );
+		this.sinaisControle.get( 9 ).modificarValor( "00" );
+		
+		this.imprimirSinaisDeControleAtivos();
+		this.inativarSinaisControle();
+		
+		System.out.println( "Acesso a memoria" );
+		if( instrucao instanceof Sw ) {
+			this.sinaisControle.get( 5 ).modificarValor( "" );
+			this.sinaisControle.get( 3 ).modificarValor( "1" );
+			
+			this.imprimirSinaisDeControleAtivos();
+			this.inativarSinaisControle();
+		}else{
+			this.sinaisControle.get( 4 ).modificarValor( "" );
+			this.sinaisControle.get( 3 ).modificarValor( "1" );
+			
+			this.imprimirSinaisDeControleAtivos();
+			this.inativarSinaisControle();
+			
+			//Escrita
+			System.out.println( "Passo de escrita" );
+			this.sinaisControle.get( 13 ).modificarValor( "0" );
+			this.sinaisControle.get( 12 ).modificarValor( "" );
+			this.sinaisControle.get( 6 ).modificarValor( "1" );
+			
+			this.imprimirSinaisDeControleAtivos();
+			this.inativarSinaisControle();
+		}
+		
+	}
+	
+	private void instrucaoTipoBeq() {
+		
+		System.out.println( "Termino do desvio condicional" );
+		this.sinaisControle.get( 11 ).modificarValor( "1" );
+		this.sinaisControle.get( 10 ).modificarValor( "00" );
+		this.sinaisControle.get( 9 ).modificarValor( "01" );
+		this.sinaisControle.get( 1 ).modificarValor( "" );
+		this.sinaisControle.get( 8 ).modificarValor( "01" );
+		
+		this.imprimirSinaisDeControleAtivos();
+		this.inativarSinaisControle();
+		
+	}
+	
+	private void instrucaoTipoJ() {
+		
+		System.out.println( "Termino do desvio incondicional" );
+		this.sinaisControle.get( 2 ).modificarValor( "" );
+		this.sinaisControle.get( 8 ).modificarValor( "10" );
+		
+		this.imprimirSinaisDeControleAtivos();
+		this.inativarSinaisControle();
+		
+	}
+	
+	private void init() {
+		
+		this.m.abrirArquivo();
+		this.m.lerDados( this.lista, this.memoriaInstrucoes, this.bancoRegistradores);
+		this.m.fecharArquivo();
+		
+		
+		this.sinaisControle.put( 1, new SinalControle( "PCEscCond" ) );
+		this.sinaisControle.put( 2, new SinalControle( "PCEsc" ) );
+		this.sinaisControle.put( 3, new SinalControle( "louD" ) );
+		this.sinaisControle.put( 4, new SinalControle( "LerMem" ) );
+		this.sinaisControle.put( 5, new SinalControle( "EscMem" ) );
+		this.sinaisControle.put( 6, new SinalControle( "MemParaReg" ) );
+		this.sinaisControle.put( 7, new SinalControle( "IREsc" ) );
+		this.sinaisControle.put( 8, new SinalControle( "FontePC" ) );
+		this.sinaisControle.put( 9, new SinalControle( "UALOp" ) );
+		this.sinaisControle.put( 10, new SinalControle( "UALFonteB" ) );
+		this.sinaisControle.put( 11, new SinalControle( "UALFonteA" ) );
+		this.sinaisControle.put( 12, new SinalControle( "EscReg" ) );
+		this.sinaisControle.put( 13, new SinalControle( "RegDst" ) );
+		
+	}
+	
+	private void inativarSinaisControle() {
+		
+		for( SinalControle sc : this.sinaisControle.values() )  {
+			sc.setAtivo( false );
+		}
+		
+	}
+	
+	private void imprimirSinaisDeControleAtivos() {
+		
+		System.out.println("Sinais de Controle: \n\n");
+		for( SinalControle sc : this.sinaisControle.values() )  {
+			if( sc.isAtivo() ) {
+				System.out.println( "Nome: " + sc.getNome() + ( !sc.getValor().equals( "" ) ? " = " + sc.getValor() + "\n" : "" ) );
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+				
+		Execucao exec = new Execucao();
+		exec.executar();
+
+	}
+
+}
---
+++ SimuladorMIPS/src/util/SinalControle.java
@@ -0,0 +1,48 @@
+package util;
+
+public class SinalControle {
+	
+	private String valor;
+	private String nome;
+	private boolean ativo;
+	
+	public SinalControle( String nome ) {
+		
+		this.valor = "";
+		this.nome = nome;
+		this.ativo = false;
+		
+	}
+	
+	public void modificarValor( String valor ) {
+		
+		this.valor = valor;
+		this.ativo = ativo;
+		
+	}
+	
+	public String getValor() {
+		return valor;
+	}
+	
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
